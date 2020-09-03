63d699367612979475cfbf973bdcd8c39310b1e9
Carol Xu <carolx715@gmail.com>
2020-09-03 13:02:08 -0400

    basic player controls

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    public int speed = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {  
+        float rotateHorizontal = Input.GetAxis("Horizontal");
+        if (rotateHorizontal != 0) {
+            transform.Rotate (new Vector3 (0, rotateHorizontal * speed, 0) * Time.deltaTime);
+        }
+    }
+}
