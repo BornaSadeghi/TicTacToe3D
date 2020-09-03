0207c6dc2d8d7d587b38bc6c4c3de6b9240051a3
Carol Xu <carolx715@gmail.com>
2020-09-03 13:06:06 -0400

    comments added to explain code

--- Assets/Scripts/CubeController.cs
+++ Assets/Scripts/CubeController.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class CubeController : MonoBehaviour
 {
+    // speed of rotation: public allows it to be changed in Unity inspector
     public int speed = 20;
-    // Start is called before the first frame update
+    // Start is called before the first frame update - unused in this case
     void Start()
     {
         
@@ -14,8 +15,11 @@ public class CubeController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {  
+        // Input.GetAxis is a built in control that takes in the horizontal arrow keys
         float rotateHorizontal = Input.GetAxis("Horizontal");
+        // if one of the horizontal arrow keys is pressed
         if (rotateHorizontal != 0) {
+            // rotate the cube (it actually calls "this.transform.Rotate" but the "this" is not necessary)
             transform.Rotate (new Vector3 (0, rotateHorizontal * speed, 0) * Time.deltaTime);
         }
     }
