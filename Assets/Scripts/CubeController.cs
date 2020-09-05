a68045a6c2807ba6d1df595dd0769a1ca9473034
Carol Xu <carolx715@gmail.com>
2020-09-05 17:01:47 -0400

    fixed infinite expanding bug

--- Assets/Scripts/CubeController.cs
+++ Assets/Scripts/CubeController.cs
@@ -19,10 +19,13 @@ public class CubeController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // on press of the F key triggers action
         if (Input.GetKeyDown(KeyCode.F)) {
             isExpanded = !isExpanded;
         }
 
+        // if isExpanded = true (cube is in expanded position), change back to cube
+        // else (cube is in cube position), change to expanded position
         if (isExpanded) {
             ChangeToCube();
         } else {
@@ -31,22 +34,30 @@ public class CubeController : MonoBehaviour
     }
     void OpenFaces() {
         // code to expand into 3 faces
+        Vector3 heading = faces[0].transform.position - endPoints[0];
         if (faces[0].transform.position != endPoints[0]) {
             faces[0].transform.position += new Vector3 (0.5f, 0.0f, 0);
             faces[2].transform.position += new Vector3 (-0.5f, 0.0f, 0);
         }
-        if (faces[2].transform.position != endPoints[1]) {
+        // checks for tolerance
+        else if (heading.magnitude <= tolerance) {
+            faces[0].transform.position = endPoints[0];
+            faces[2].transform.position = endPoints[1];
         }
         // sets isExpanded to false for next time
         isExpanded = false;
     }
     void ChangeToCube() {
         // add the respective Vector3 to each face until it reaches the new point
+        Vector3 heading = faces[0].transform.position - startPoints[0];
         if (faces[0].transform.position != startPoints[0]) {
             faces[0].transform.position += new Vector3 (-0.5f, 0.0f, 0);
             faces[2].transform.position += new Vector3 (0.5f, 0.0f, 0);
         }
-        if (faces[2].transform.position != startPoints[1]) {
+        // checks for tolerance
+        else if (heading.magnitude <= tolerance) {
+            faces[0].transform.position = startPoints[0];
+            faces[2].transform.position = startPoints[1];
         }
         // sets isExpanded to true for next time
         isExpanded = true;
